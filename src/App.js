a9cc077a93875b194747d5a0583cd4a05ac7356b
ChoudaryAzeem <a5555azeem@gmail.com>
2024-01-24 14:54:54 +0500

    react app commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe.jsx';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
